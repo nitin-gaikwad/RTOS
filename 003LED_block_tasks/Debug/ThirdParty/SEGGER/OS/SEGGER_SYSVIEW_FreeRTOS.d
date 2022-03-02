72567fecbd6b35806d9cf9912d42bcdcf17e5c02
nitin_gaikwad <ngvirtue@gmail.com>
2022-03-02 21:56:33 +0530

    Led blinking using Blocked delay(vTaskDelay)

---
+++ 003LED_block_tasks/Debug/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.d
@@ -0,0 +1,53 @@
+ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.o: \
+ ../ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.c \
+ F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/SEGGER/SEGGER/SEGGER.h \
+ F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/SEGGER/Config/Global.h \
+ F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h \
+ F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h \
+ F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h \
+ F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/include/projdefs.h \
+ F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/include/portable.h \
+ F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/include/task.h \
+ F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/include/list.h \
+ ../ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h
+
+F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+
+F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+
+F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+
+F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+
+F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/SEGGER/SEGGER/SEGGER.h:
+
+F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/SEGGER/Config/Global.h:
+
+F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h:
+
+F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h:
+
+F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
+
+F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/include/projdefs.h:
+
+F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/include/portable.h:
+
+F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+
+F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+
+F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+
+F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/include/task.h:
+
+F:/Ni3/RTOS_STM32F4/003LED_block_tasks/ThirdParty/FreeRTOS/include/list.h:
+
+../ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
